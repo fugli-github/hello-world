3dd5ce0eb92b35c78eb05a541c0cb63171bcac25
fugli <fugui.li@nokia-sbell.com>
2019-11-28 18:47:13 +0800

    remove using namespace std
    Signed-off-by: fugli <fugui.li@nokia-sbell.com>

--- test.cpp
+++ test.cpp
@@ -1,11 +1,9 @@
 #include "iostream"
 
-using namespace std;
 
-int
-main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 
-    cout<<"hello world"<<endl;
+	std::cout<<"hello world"<<std::endl;
 
 }
 
